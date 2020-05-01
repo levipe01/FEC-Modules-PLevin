a063ef4e9bb33a9aa2b6f3169dc7036295ccc503
Peter Levin <levipe01@gmail.com>
2020-05-01 16:16:43 -0600

    Refactored db seed script for clarity

--- db/seed_db.js
+++ db/seed_db.js
@@ -17,12 +17,8 @@ function getEntriesCount() {
 }
 
 function titleCase(string) {
-  let output = string.toLowerCase();
-  output = output.split(' ');
-
-  for (let i = 0; i < output.length; i += 1) {
-    output[i] = output[i].charAt(0).toUpperCase() + output[i].slice(1);
-  }
+  let output = string.toLowerCase().split(' ');
+  output = output.map((word) => word.charAt(0).toUpperCase() + word.slice(1));
   return output.join(' ');
 }
 
@@ -47,8 +43,8 @@ function generateRandomFeedback(entries) {
 
 function seedProducts(entries) {
   const output = [];
+  const query = 'INSERT INTO products(name, product_url, image_url, is_prime, price) VALUES($1, $2, $3, $4, $5)';
   for (let i = 0; i < entries; i += 1) {
-    const query = 'INSERT INTO products(name, product_url, image_url, is_prime, price) VALUES($1, $2, $3, $4, $5)';
     const values = generateRandomProduct();
     output.push(db.query(query, values));
   }
@@ -57,8 +53,8 @@ function seedProducts(entries) {
 
 function seedFeedback(entries) {
   const output = [];
+  const query = 'INSERT INTO feedback(type_id, comments, prod_id, user_name) VALUES($1, $2, $3, $4)';
   for (let i = 0; i < entries; i += 1) {
-    const query = 'INSERT INTO feedback(type_id, comments, prod_id, user_name) VALUES($1, $2, $3, $4)';
     const values = generateRandomFeedback(entries);
     output.push(db.query(query, values));
   }
@@ -66,17 +62,17 @@ function seedFeedback(entries) {
 }
 
 function createAndSeedFeedback(entries) {
-  const query = 'CREATE TABLE feedback(id BIGSERIAL PRIMARY KEY,type_id INTEGER,comments VARCHAR(1000),prod_id INTEGER,user_name VARCHAR(50), FOREIGN KEY (prod_id) REFERENCES products (id));';
+  const createTableQuery = 'CREATE TABLE feedback(id BIGSERIAL PRIMARY KEY,type_id INTEGER,comments VARCHAR(1000),prod_id INTEGER,user_name VARCHAR(50), FOREIGN KEY (prod_id) REFERENCES products (id));';
   return db.query('DROP TABLE IF EXISTS feedback')
-    .then(() => db.query(query))
+    .then(() => db.query(createTableQuery))
     .then(() => seedFeedback(entries));
 }
 
 function createAndSeedProducts(entries) {
-  const query = 'CREATE TABLE products(id BIGSERIAL PRIMARY KEY,name VARCHAR(200) NOT NULL,product_url VARCHAR(200),image_url VARCHAR(200),is_prime BOOLEAN NOT NULL,price DECIMAL NOT NULL);';
+  const createTableQuery = 'CREATE TABLE products(id BIGSERIAL PRIMARY KEY,name VARCHAR(200) NOT NULL,product_url VARCHAR(200),image_url VARCHAR(200),is_prime BOOLEAN NOT NULL,price DECIMAL NOT NULL);';
 
   return db.query('DROP TABLE IF EXISTS products CASCADE')
-    .then(() => db.query(query))
+    .then(() => db.query(createTableQuery))
     .then(() => db.query('ALTER SEQUENCE products_id_seq RESTART WITH 1000'))
     .then(() => seedProducts(entries));
 }
@@ -85,5 +81,5 @@ getEntriesCount()
   .then((entries) => createAndSeedProducts(entries))
   .then(() => db.query('SELECT * FROM products'))
   .then((results) => createAndSeedFeedback(results.rows.length))
-  .then(() => process.exit())
+  .finally(() => process.exit())
   .catch((e) => e);
