1cf91cf85138c0934f539884a3d04acaf313dfe8
Peter Levin <levipe01@gmail.com>
2020-05-06 20:12:52 -0600

    Added two unit test

--- _tests_/App.test.jsx
+++ _tests_/App.test.jsx
@@ -1,12 +1,30 @@
 /* eslint-env jest */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../client/src/components/App.jsx';
+import Footer from '../client/src/components/Footer.jsx';
 
 
 describe('Unit Tests', () => {
   test('should render the app component on the screen', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toExist();
+  });
+});
+
+describe('Unit Interaction Tests', () => {
+  test('it should correctly update the state when resetCarouse is called', () => {
+    const wrapper = mount(<App />);
+    const mockSubmitHandler = () => {
+      wrapper.setState({ feedbackVisible: true });
+    };
+    const header = shallow(
+      <Footer
+        feedbackVisible={false}
+        toggleFeedback={mockSubmitHandler}
+      />,
+    );
+    header.find('.feedbackButton').simulate('click');
+    expect(wrapper.instance().state.feedbackVisible).toBe(true);
   });
 });
--- _tests_/Footer.test.jsx
+++ _tests_/Footer.test.jsx
@@ -6,7 +6,24 @@ import Footer from '../client/src/components/Footer.jsx';
 
 describe('Footer Unit Tests', () => {
   test('should render the Footer Component', () => {
-    const wrapper = shallow(<Footer />);
+    const wrapper = shallow(<Footer
+      feedbackVisible={true}
+      toggleFeedback={() => {}}/>);
     expect(wrapper).toExist();
+  });
+});
+
+describe('Toggle Feedback', () => {
+  test('should invoke the toggleFeedback prop when the start over button is clicked', () => {
+    const mockToggleFeedback = jest.fn();
+    const wrapper = shallow(
+      <Footer
+        feedbackVisible={true}
+        toggleFeedback={mockToggleFeedback}
+      />,
+    );
+    const updateButton = wrapper.find('.feedbackButton');
+    updateButton.simulate('click');
+    expect(mockToggleFeedback).toHaveBeenCalled();
   });
 });
